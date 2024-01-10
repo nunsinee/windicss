0a92bab4384bd49f03dd14bd3362a1c032b67c72
Nunsinee Viken <nunsinee@gmail.com>
2024-01-10 13:39:48 +0100

    First commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# windicss
-Learning Windi Css
---
+++ windi.config.js
@@ -0,0 +1,5 @@
+export default {
+	shortcuts: {
+		btn: "self-end cursor-pointer px-6 py-3 border border-transparent rounded-md text-light-100 bg-fuchsia-800 hover:(bg-pink-800 border-1 border-black-500)",
+	},
+};
